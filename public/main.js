3e7b4f9d99c1bcaea625cf9d1b52fbac246dfb4d
utkarsh4321 <utkarshsrivastava434@gmail.com>
2020-11-30 23:41:03 +0530

    added dark mode with os prefernce and manauly

--- public/main.js
+++ public/main.js
@@ -1,3 +1,20 @@
+document.addEventListener('DOMContentLoaded', () => {
+  if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+    document.documentElement.classList.add('dark');
+  } else {
+    document.documentElement.classList.remove('dark');
+
+  }
+
+})
+
+
 document.getElementById('toogleBtn').addEventListener('click', () => {
+  if (document.documentElement.classList.contains('dark')) {
+    localStorage.theme = 'light';
+  } else {
+    localStorage.theme = 'dark';
+
+  }
   document.documentElement.classList.toggle('dark');
 })
